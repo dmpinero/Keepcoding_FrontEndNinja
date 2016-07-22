ef84871cd5fd87cf4cf8722eb01783d2a97832ce
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-07-22 14:20:46 +0200

    Sexto commit. SASS y Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'); // Importa gulp de la carpeta node_modules
 var sass = require('gulp-sass'); // Importa gulp-sass
+var notify = require('gulp-notify'); // Notificaciones de escritorio
 
 // Definimos tarea por defecto
 gulp.task("default", function(){
@@ -11,4 +12,13 @@ gulp.task("compile-sass", function(){
 	gulp.src("./src/scss/style.scss") // cargamos el archivo
 	.pipe(sass().on('error', sass.logError)) // compilamos el archivo sass. En caso de error lo muestra por el log
 	.pipe(gulp.dest("./dist/css/")) // guardamos el archivo en dist/css
+	.pipe(notify({
+		title: "SASS",
+		message: "Compiled!!"
+	}));
 });
+
+// Notificaciones de escritorio
+
+
+
