2b9fdc714839d7fdba91322afe7bffdc206c4e9a
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-07-22 11:40:53 +0200

    Quinto commit. Configuración de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var sass = require('gulp-sass'); // Importa gulp-sass
 
 // Definimos tarea por defecto
 gulp.task("default", function(){
-	console.log("Hello World");
+	gulp.watch("./src/scss/*.scss", ["compile-sass"]); // Observa cambios en SASS , si se producen se ejecuta la tarea compile-sass
 });
 
 // Definimos tarea para compiplar SASS
 gulp.task("compile-sass", function(){
 	gulp.src("./src/scss/style.scss") // cargamos el archivo
-	.pipe(sass()) // compilamos el archivo sass
+	.pipe(sass().on('error', sass.logError)) // compilamos el archivo sass. En caso de error lo muestra por el log
 	.pipe(gulp.dest("./dist/css/")) // guardamos el archivo en dist/css
 });
