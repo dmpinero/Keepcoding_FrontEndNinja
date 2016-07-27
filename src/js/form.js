5a19a68bc8bae929092613d1ba7f8b79b2b1e99e
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-07-27 14:35:52 +0200

    Añadido server.py (sparREST) y click en el botón del formulario para detectarlo con jQuery

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,11 @@
 var $ = require('jquery');
 
-console.log("Hello form");
+console.log("Me cargo");
+
+// Al hacer click en el botón de guardar enviamos una petición Ajax
+// para almacenar la canción
+$('.new-song-form button').on("click", function() {
+	console.log("Click en el botón");
+
+	return false; // == preventDefault
+});
